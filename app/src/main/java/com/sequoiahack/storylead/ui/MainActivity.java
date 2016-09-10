3f632c7c5e9bceff910694eace240c526cbf4724
Zacharias Manuel <zacflys@gmail.com>
2016-09-10 12:01:49 +0530

    Initial commit

---
+++ app/src/main/java/com/sequoiahack/storylead/ui/MainActivity.java
@@ -0,0 +1,15 @@
+package com.sequoiahack.storylead.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.sequoiahack.storylead.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
